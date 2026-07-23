ad69bc062f898db44e838b69de26621864c9efbd
monika-oss <monikababu18@gmail.com>
2026-07-23 20:01:30 +0530

    Optimize chunk splitting to reduce TBT and script evaluation time

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,24 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor-react';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-framer-motion';
+            }
+            if (id.includes('three') || id.includes('@react-three')) {
+              return 'vendor-three';
+            }
+            return 'vendor'; // all other dependencies
+          }
+        }
+      }
+    }
+  }
 })
